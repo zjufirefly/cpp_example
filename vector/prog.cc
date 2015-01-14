a9784e8a86ea9768718e5a0934170b20991b4cf9
zjufirefly <zjufirefly@126.com>
2015-01-14 11:43:26 +0800

    set map

--- vector/prog.cc
+++ vector/prog.cc
@@ -2,6 +2,8 @@
 #include <vector>
 #include <deque>
 #include <list>
+#include <set>
+#include <map>
 
 using namespace std;
 
@@ -44,6 +46,35 @@ int main(int argc, char* argv[])
     while (!col3.empty()) {
         cout << col3.front() << ' ';
         col3.pop_front();
+    }
+    cout << endl;
+
+    set<int, greater<int> > col4;
+
+    col4.insert(3);
+    col4.insert(1);
+    col4.insert(5);
+    col4.insert(4);
+
+    set<int>::const_iterator pos1;
+    for (pos1 = col4.begin(); pos1 != col4.end(); ++pos1) {
+        cout << *pos1 << ' ';
+    }
+    cout << endl;
+
+    multimap<int, string> col5;
+
+    col5.insert(make_pair(5, "tagged"));
+    col5.insert(make_pair(2, "a"));
+    col5.insert(make_pair(1, "this"));
+    col5.insert(make_pair(4, "of"));
+    col5.insert(make_pair(6, "strings"));
+    col5.insert(make_pair(1, "is"));
+    col5.insert(make_pair(3, "multimap"));
+
+    multimap<int, string>::const_iterator pos2;
+    for (pos2 = col5.begin(); pos2 != col5.end(); ++pos2) {
+        cout << pos2->second <<  ' ';
     }
     cout << endl;
 
